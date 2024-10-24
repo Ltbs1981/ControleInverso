0d1c0c661b50d13b25e6319b6d62fa09321b55d3
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-24 13:01:43 -0300

    feito o controller

--- Servicos/ClienteService.cs
+++ Servicos/ClienteService.cs
@@ -5,7 +5,31 @@ using ControleInverso.Core.Contratos.Servicos;
 using ControleInverso.Infra.Repositorios;
 namespace ControleInverso.Servicos
 {
-    internal class ClienteService: IClienteService
+    internal class ClienteService : IClienteService
     {
+        private readonly IClienteRepositorio _clienteRepositorio;
+
+        public ClienteService(IClienteRepositorio clienteRepositorio)
+        {
+            _clienteRepositorio = clienteRepositorio;
+        }
+
+        public Cliente GetCliente()
+        {
+            return new Cliente();
+        }
+
+        public void AdicionaCliente(Cliente cliente)
+        {
+            if (cliente == null)
+            {
+                Console.WriteLine("Cliente invalido");
+            }
+
+            _clienteRepositorio.AdicionaCliente(cliente);
+            //salva no banco 
+            //Envia Email
+        }
+
     }
 }
---
+++ Web/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using ControleInverso.Core.Entidades;
+using ControleInverso.Infra.Repositorios;
+using ControleInverso.Servicos;
+using ControleInverso.Core.Contratos.Repositorios;
+using ControleInverso.Core.Contratos.Servicos;
+
+namespace ControleInverso.Web.Controllers
+{
+    internal class ClienteController
+    {
+        private IClienteService _clienteService;
+        public void AdicionaCliente(string nome, DateTime nascimento, string email)
+        {
+            Cliente cliente = new Cliente(nome, nascimento, email);
+
+            _clienteService = new ClienteService(new ClienteRepositorio());
+            _clienteService.AdicionaCliente(cliente);
+        }
+    }
+}
